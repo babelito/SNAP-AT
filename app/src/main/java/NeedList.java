583223b72ba692f45b9f23d5b77b6efe2872c52e
ArthurFort <arthur.fort@qgandco.fr>
2017-10-02 16:12:38 +0200

    Second Commit

---
+++ app/src/main/java/NeedList.java
@@ -0,0 +1,24 @@
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.support.v4.media.RatingCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+/**
+ * Created by Arthur on 02/10/2017.
+ */
+
+public class NeedList extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
